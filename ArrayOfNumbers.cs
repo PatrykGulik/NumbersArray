4fadc6d7b92207df1cfeb9400974984ef05629a8
gulik93 <11002010@uhi.ac.uk>
2026-02-03 20:24:18 +0000

    Task 1
    Initialise new array, sizeOfArray and constructors

---
+++ ArrayOfNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class ArrayOfNumbers
+{
+	public ArrayOfNumbers()
+	{
+
+		private int[] array = new int[_sizeOfArray];
+
+		private int _sizeOfArray;
+
+		//Constructors
+		public ArrayOfNumbers(int sizeOfArray)
+		{
+			_sizeOfArray = sizeOfArray;
+			array = { 0 };
+		}
+
+		public ArrayOfNumbers(int[] array)
+		{
+			int[] localArray = array;
+		}
+
+
+	}
+}
